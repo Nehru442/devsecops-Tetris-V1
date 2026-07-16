c8592b28bf9420873685b1e707da3e44cac24534
Nehru442 <nehru.k1411@gmail.com>
2026-07-16 16:20:41 +0530

    Update S3 bucket name in backend configuration

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "devsecops-tetris-nehru-730" # Replace with your actual S3 bucket name
+    bucket = "devsecops-tetris-nehru-111" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
